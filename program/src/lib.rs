17b35bcaddb7ecb4c1f89ee888553cb2902c51ff
aoikurokawa <ht.so.ik0102@gmail.com>
2026-04-27 08:59:35 +0900

    fix: update

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -2,7 +2,7 @@ use pinocchio::{entrypoint, AccountView, Address, ProgramResult};
 
 entrypoint!(process_instruction);
 
-// pinocchio_pubkey::declare_id!("Lann5fHSrV7sswqd5r55n5hN43DHcmveMZGpY7EufP5");
+pinocchio_pubkey::declare_id!("Lann5fHSrV7sswqd5r55n5hN43DHcmveMZGpY7EufP5");
 
 fn process_instruction(
     _program_id: &Address,
