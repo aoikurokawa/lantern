4bb0dbcd305e07817ea166e453c48182ce032f94
aoikurokawa <ht.so.ik0102@gmail.com>
2026-04-27 08:51:22 +0900

    fix: update

---
+++ program/src/lib.rs
@@ -0,0 +1,13 @@
+use pinocchio::{AccountView, Address, ProgramResult, entrypoint};
+
+entrypoint!(process_instruction);
+
+pinocchio_pubkey::declare_id!("Lann5fHSrV7sswqd5r55n5hN43DHcmveMZGpY7EufP5");
+
+fn process_instruction(
+    _program_id: &Address,
+    _accounts: &[AccountView],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
--- src/merkle_tree/mod.rs
+++
@@ -1,3 +0,0 @@
-mod standard_binary_tree;
-
-pub use standard_binary_tree::MerkleTree;
